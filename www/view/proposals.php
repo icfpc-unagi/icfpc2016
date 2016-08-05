071b8698252ab63361da0ddc36d2bb5c3c22f35b
imos <git@imoz.jp>
2016-08-06 00:03:11 +0900

    Add proposals page.

---
+++ www/view/proposals.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../library/api.php');
+
+?><html>
+<meta charset="UTF-8">
+<title>Proposals</title>
+<link rel="stylesheet" type="text/css" href="/style.css">
+<body>
+<h1>Proposals</h1>
+<table>
+<tr><td>Problem ID</td><td>Problem Size</td><td>Owner's Solution Size</td><td>Publish Time</td><td>Edit</td></tr>
+<?php
+
+foreach (Database::Select('
+    SELECT
+        `proposal_id`,
+        `problem_id`,
+        `proposal_note`,
+        LENGTH(`proposal_data`) AS `problem_size`,
+        LENGTH(`proposal_solution`) AS `solution_size`,
+        `proposal_submission`,
+        `proposal_modified`
+    FROM `proposal`
+    ORDER BY `proposal_id`') AS $proposal) {
+  echo '<tr>';
+  if (is_null($proposal['problem_id'])) {
+    echo '<td>-</td>';
+  } else {
+    echo '<td><a href="problem.php?problem_id=' . $proposal['problem_id'] .
+       '">' . $proposal['problem_id'] . "</a></td>";
+  }
+  echo '<td>' . $proposal['problem_size'] . "</td>";
+  echo '<td>' . $proposal['solution_size'] . "</td>";
+  echo '<td>' . $solved . '</td>';
+  echo '<td>' . date('Y-m-d H:i:s', $proposal['proposal_id']) . "</td>";
+  echo '<td><a href="proposal.php?proposal_id=' . $proposal['proposal_id'] .
+       '">Edit</a></td>';
+  echo "</tr>\n";
+}
+
+?>
+</table>
+</body>
---
+++ www/view/submit.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../library/api.php');
+
+function FindSolution() {
+  Database::Command('
+      UPDATE `solution`
+      SET `solution_id` = (@solution_id := `solution_id`),
+          `solution_lock` = UNIX_TIMESTAMP() + 60
+      WHERE
+          `solution_lock` < UNIX_TIMESTAMP() AND
+          `solution_submission` IS NULL
+      ORDER BY `solution_lock`
+      LIMIT 1');
+  if (Database::AffectedRows() != 0) { return TRUE; }
+  return FALSE;
+}
+
+function Submit($solution) {
+  $response = SubmitSolution(
+      $solution['problem_id'], $solution['solution_data']);
+  if (is_null($response)) {
+    die("Failed to submit.");
+  }
+  Database::Command('
+      UPDATE `solution`
+      SET `solution_resemblance` = {solution_resemblance},
+          `solution_submission` = CURRENT_TIMESTAMP(),
+          `solution_response` = {solution_response}
+      WHERE `solution_id` = {solution_id}',
+      ['solution_id' => $solution['solution_id'],
+       'solution_resemblance' => $response['resemblance'],
+       'solution_response' => json_encode($response)]);
+}
+
+if (!FindSolution()) {
+  die('No submission is pending.');
+}
+
+Submit(Database::SelectRow('
+    SELECT `solution_id`, `problem_id`, `solution_data`
+    FROM `solution`
+    WHERE `solution_id` = @solution_id'));
