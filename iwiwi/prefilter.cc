dd63e5357d3a0e3adaa39f9152899528dd661558
Takuya Akiba <t.akiba.65536@gmail.com>
2016-08-05 20:09:39 +0900

    postfilterの作業スタートのための整理

--- iwiwi/prefilter.cc
+++ iwiwi/prefilter.cc
@@ -1,59 +1,4 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <set>
-#include <map>
-#include <algorithm>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cctype>
-#include <cmath>
-#include <cassert>
-#include <sstream>
-
-#include <boost/assert.hpp>
-#include <boost/geometry.hpp>
-#include <boost/geometry/geometries/linestring.hpp>
-#include <boost/geometry/geometries/point_xy.hpp>
-#include <boost/geometry/geometries/geometries.hpp>
-#include <boost/geometry/algorithms/intersection.hpp>
-#include <boost/assign/list_of.hpp>
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/multiprecision/gmp.hpp>
-#include <boost/geometry/algorithms/area.hpp>
-
-#define all(c) (c).begin(), (c).end()
-#define iter(c) __typeof((c).begin())
-#define cpresent(c, e) (find(all(c), (e)) != (c).end())
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define tr(c, i) for (iter(c) i = (c).begin(); i != (c).end(); ++i)
-#define pb(e) push_back(e)
-#define mp(a, b) make_pair(a, b)
-
-using namespace std;
-namespace bm = boost::multiprecision;
-namespace bg = boost::geometry;
-
-// using Bigrat = boost::multiprecision::cpp_rational;
-using Bigrat = bm::number<bm::gmp_rational, bm::et_off>;
-// using Bigrat = bm::number<bm::cpp_rational, bm::et_off>;
-using Point = bg::model::d2::point_xy<Bigrat>;
-using Segment = bg::model::segment<Point>;
-using Polygon = bg::model::polygon<Point, false>;
-
-Point ReadPoint(istream &is) {
-  Bigrat x, y;
-  is >> x >> y;
-  return Point(x, y);
-}
-
-Segment ReadSegment(istream &is) {
-  return Segment(ReadPoint(is), ReadPoint(is));
-}
+#include "common.h"
 
 /*
 Polygon ReadPolygon(istream &is) {
@@ -82,13 +27,7 @@ vector<vector<int>> adj;
 vector<vector<int>> regions;
 
 void Input() {
-  string in;
-  for (string line; getline(cin, line); ) {
-    in += line + "\n";
-  }
-  for (char &c : in) if (c == ',') c = ' ';
-
-  istringstream ss(in);
+  istringstream ss(ReadAllAndRemoveComma(cin));
 
   int num_polygons;
   ss >> num_polygons;
@@ -191,18 +130,6 @@ Polygon RegionPolygon(int k) {
   Polygon p;
   bg::assign_points(p, ps);
   return p;
-}
-
-int Quadrant(const Point &p) {
-  assert(p.x() != 0 || p.y() != 0);
-  if (p.y() >= 0 && p.x() > 0) return 0;
-  if (p.x() <= 0 && p.y() > 0) return 1;
-  if (p.y() <= 0 && p.x() < 0) return 2;
-  else return 3;
-}
-
-Bigrat Det(const Point &a, const Point &b) {
-  return a.x() * b.y() - a.y() * b.x();
 }
 
 void EnumerateRegions() {
