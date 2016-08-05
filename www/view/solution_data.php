17b9a0c56da2f18e3bf936eb741062ce8cdcf67d
imos <git@imoz.jp>
2016-08-05 20:52:16 +0900

    Add solution viewer.

---
+++ www/view/solution_data.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../library/api.php');
+
+$solution_id = NULL;
+if (isset($_GET['solution_id'])) {
+  $solution_id = $_GET['solution_id'];
+} else if (isset($_ENV['solution_id'])) {
+  $solution_id = $_ENV['solution_id'];
+} else {
+  die('solution_id must be given.');
+}
+
+$result = Database::SelectCell('SELECT `solution_data` FROM `solution` WHERE `solution_id` = {solution_id}', ['solution_id' => $solution_id]);
+
+if ($result === FALSE) {
+  die('Solution is not found.');
+}
+
+header('Context-Type: text/plain');
+echo $result;
