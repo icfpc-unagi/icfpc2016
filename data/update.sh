8bb790d1acaf8efc6457dedcaaf6f99ba0d143a2
imos <git@imoz.jp>
2016-08-06 14:33:33 +0900

    Add updater.

---
+++ data/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")"
+
+php fetch.php
+bazel build -c opt //iwiwi:prefilter
+
+pushd problems
+for file in *.txt; do
+  ../../bazel-bin/iwiwi/prefilter < $file > ../problems_converted/$file
+done
+popd
+
+git add problems/*.txt problems_converted/*.txt
