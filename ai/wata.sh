39edfd16cc6d9004772bf3431703569eb960afc1
imos <git@imoz.jp>
2016-08-06 11:46:48 +0900

    Apply prefilter/postfilter to wata.

--- ai/wata.sh
+++ ai/wata.sh
@@ -2,4 +2,7 @@
 
 set -e -u
 
-java -jar "${BASH_SOURCE}.runfiles/wata/wata_deploy.jar"
+"${BASH_SOURCE}.runfiles/iwiwi/prefilter" |
+java -jar "${BASH_SOURCE}.runfiles/wata/wata_deploy.jar" |
+"${BASH_SOURCE}.runfiles/iwiwi/postfilter1" |
+"${BASH_SOURCE}.runfiles/iwiwi/postfilter2"
