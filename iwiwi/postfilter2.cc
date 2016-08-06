08926dbc1bd5adb468f78097488b763e38ad4a43
Takuya Akiba <t.akiba.65536@gmail.com>
2016-08-07 00:45:39 +0900

    回転を試して一番短い物を出力

--- iwiwi/postfilter2.cc
+++ iwiwi/postfilter2.cc
@@ -2,10 +2,15 @@
 
 #include "common.h"
 
+// Input
 int num_points, num_facets;
 vector<Point> src, dst;
 vector<vector<int>> facets;
 vector<Polygon> src_polygons, dst_polygons;
+
+// Relabel
+vector<int> old_to_new;
+vector<int> new_to_old;
 
 void Input() {
   istringstream is(ReadAllAndRemoveComma(cin));
@@ -28,6 +33,35 @@ void Input() {
 }
 
 
+void Output(ostream &os, bool flip_x, bool flip_y, bool swap_xy) {
+  os << new_to_old.size() << endl;
+  rep (k, new_to_old.size()) {
+    int i = new_to_old[k];
+    // transformation
+    Bigrat x = src[i].x(), y = src[i].y();
+    if (flip_x) x = 1 - x;
+    if (flip_y) y = 1 - y;
+    if (swap_xy) swap(x, y);
+    os << x << "," << y << endl;
+  }
+  os << facets.size() << endl;
+  rep (i, facets.size()) {
+    vector<int> new_facet;
+    for (int v : facets[i]) {
+      if (old_to_new[v] != -1) new_facet.emplace_back(old_to_new[v]);
+    }
+
+    os << new_facet.size();
+    for (int v : new_facet) os << " " << v;
+    os << endl;
+  }
+  rep (k, new_to_old.size()) {
+    int i = new_to_old[k];
+    os << dst[i].x() << "," << dst[i].y() << endl;
+  }
+}
+
+
 int main() {
   Input();
 
@@ -44,7 +78,7 @@ int main() {
     a.erase(unique(all(a)), a.end());
   }
 
-  vector<int> old_to_new(num_points);
+  old_to_new.assign(num_points, 0);
   rep (v, num_points) {
     const Point &p = src[v];
     if (adj[v].empty()) {
@@ -59,7 +93,7 @@ int main() {
   }
 
   // Relabeling
-  vector<int> new_to_old;
+  new_to_old.clear();
   rep (v, num_points) {
     if (old_to_new[v] != -1) {
       new_to_old.emplace_back(v);
@@ -79,24 +113,13 @@ int main() {
        << new_num_points << endl;
 
   // Output
-  cout << new_num_points << endl;
-  rep (k, new_num_points) {
-    int i = new_to_old[k];
-    cout << src[i].x() << "," << src[i].y() << endl;
+  string out;
+  rep (flip_x, 2) rep (flip_y, 2) rep (swap_xy, 2) {
+    ostringstream oss;
+    Output(oss, flip_x, flip_y, swap_xy);
+    string tmp = oss.str();
+    // cerr << tmp.length() << endl;
+    if (out.empty() || out.length() > tmp.length()) out = tmp;
   }
-  cout << facets.size() << endl;
-  rep (i, facets.size()) {
-    vector<int> new_facet;
-    for (int v : facets[i]) {
-      if (old_to_new[v] != -1) new_facet.emplace_back(old_to_new[v]);
-    }
-
-    cout << new_facet.size();
-    for (int v : new_facet) cout << " " << v;
-    cout << endl;
-  }
-  rep (k, new_num_points) {
-    int i = new_to_old[k];
-    cout << dst[i].x() << "," << dst[i].y() << endl;
-  }
+  cout << out;
 }
