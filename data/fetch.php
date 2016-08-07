0e76832b468e03b978dec5664cb214995328ffbe
imos <git@imoz.jp>
2016-08-07 14:20:35 +0900

    Use internal API to fetch problems.

--- data/fetch.php
+++ data/fetch.php
@@ -1,51 +1,22 @@
 <?php
 
-function Fetch($path, $params = NULL) {
-  $header = [
-      'X-API-Key: 42-59d13e150cd0de2292ef5adf36cb1e26',
-      'Expect: ',
-      'Accept-Encoding: gzip',
-  ];
-  $http = [
-      'method'  => is_null($params) ? 'GET' : 'POST',
-      'header'  => implode("\r\n", $header),
-  ];
-  if (!is_null($params)) {
-    $params = http_build_query($params, '', '&');
-    $header[] = 'Content-Type: application/x-www-form-urlencoded';
-    $header[] = 'Content-Length: ' . strlen($params);
-    $http['content'] = $params;
-  };
-
-  $url = "http://2016sv.icfpcontest.org/api${path}";
-  for ($i = 0; $i < 3; $i++) {
-    $result = file_get_contents(
-        $url, false, stream_context_create(['http' => $http]));
-    if ($result) break;
-    fwrite(STDERR, "Failed to get.\n");
-    sleep(pow(2, $i));
-  }
-  return gzdecode($result);
-}
-
 function Prettify($data) {
   return json_encode(json_decode($data), JSON_PRETTY_PRINT);
 }
 
-$list = json_decode(Fetch('/snapshot/list'), TRUE);
-$snapshots = [];
-foreach ($list['snapshots'] as $snapshot) {
-  $snapshots[$snapshot['snapshot_time']] = $snapshot['snapshot_hash'];
-}
-ksort($snapshots);
-$snapshot = array_pop($snapshots);
+date_default_timezone_set('Asia/Tokyo');
 $path = dirname(__FILE__) . '/snapshot.json';
-file_put_contents($path, Prettify(Fetch("/blob/$snapshot")));
+file_put_contents($path,
+    Prettify(file_get_contents('http://db.sx9.jp/view/snapshot.php?plain=1')));
 
 $contest = json_decode(file_get_contents($path), TRUE);
+echo "Snapshot at " . date('Y-m-d H:i:s', $contest['snapshot_time']) . "\n";
 foreach ($contest['problems'] as $problem) {
   $path = dirname(__FILE__) . '/problems/' . $problem['problem_id'] . '.txt';
   if (file_exists($path)) continue;
   sleep(1);
-  file_put_contents($path, Fetch("/blob/{$problem['problem_spec_hash']}"));
+  echo "Fetching {$problem['problem_spec_hash']}...\n";
+  file_put_contents($path, file_get_contents(
+      'http://db.sx9.jp/api/blob.php?blob_id=' .
+      $problem['problem_spec_hash']));
 }
