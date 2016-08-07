663196c76deccfa3179ac01ee861604ddf9ea9df
imos <git@imoz.jp>
2016-08-07 18:11:03 +0900

    Add zenkan counter.

--- www/view/snapshot.php
+++ www/view/snapshot.php
@@ -26,18 +26,9 @@ ksort($users);
 
 StartPage();
 
+ob_start();
+
 ?>
-<h1 class="page-header">Problems at <?php
-echo date('Y-m-d H:i:s', $snapshot['snapshot_time']);
-?></h1>
-<div><?php
-$full = !is_null(GetParameter('full'));
-if (!$full) {
-  echo '[<a href="?full=1">Full</a>]';
-} else {
-  echo '[<a href="?">Compact</a>]';
-}
-?></div>
 <table class="table table-condensed table-striped">
   <tbody>
 <tr><th<?php if (!$full) echo ' colspan="2"'; ?>>ID</th><th>Owner</th><th>Problem Size</th><th>Owner's Solution Size</th><th>Solved</th><th>Solutions</th><th>Publish Time</th></tr>
@@ -64,6 +55,7 @@ foreach (Database::Select('
   $solutions[$problem['problem_id']]['all'] = $problem;
 }
 
+$not_solved = 0;
 foreach ($snapshot['problems'] as $problem) {
   $solved = 0;
   $resemblance = 0.0;
@@ -75,9 +67,11 @@ foreach ($snapshot['problems'] as $problem) {
     }
   }
   $solution = $solutions[intval($problem['problem_id'])];
-  if (!$full) {
-    if ($solution['all']['solution_resemblance'] == 1) continue;
-    if ($problem['owner'] == '42') continue;
+  if ($solution['all']['solution_resemblance'] == 1 ||
+      $problem['owner'] == '42') {
+    if (!$full) continue;
+  } else {
+    $not_solved++;
   }
   echo '<tr>';
   echo '<td><a href="problem.php?problem_id=' . $problem['problem_id'] .
@@ -116,6 +110,24 @@ foreach ($snapshot['problems'] as $problem) {
 
 ?>
 </table>
+<?php
+
+$contents = ob_get_clean();
+
+?>
+<div style="font-size:400%;font-weight:bold;text-align:center;">☆☆☆　全完まで<?php echo $not_solved; ?>問！　☆☆☆</div>
+<h1 class="page-header">Problems at <?php
+echo date('Y-m-d H:i:s', $snapshot['snapshot_time']);
+?></h1>
+<div><?php
+$full = !is_null(GetParameter('full'));
+if (!$full) {
+  echo '[<a href="?full=1">Full</a>]';
+} else {
+  echo '[<a href="?">Compact</a>]';
+}
+?></div>
+<?php echo $contents; ?>
 <h1 class="page-header">Leaderboard</h1>
 <table class="table table-condensed table-striped">
   <tbody>
